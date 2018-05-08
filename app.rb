8a20b3fa4e5f9b6efc7e7835755ef3d1045dce21
Stephen Harrington <stephenharrington04@yahoo.com>
2018-05-08 17:57:55 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,12 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get "/" do
+  get '/' do
 
     erb :user_input
   end
 
-  post "/piglatinize" do
+  post '/piglatinize' do
     text_from_user = params[:user_phrase]
     @piglatinized_text = PigLatinizer.new.piglatinize(text_from_user)
 
