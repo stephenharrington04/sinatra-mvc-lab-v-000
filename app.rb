225753d9f6f32ad2b9c40f592c68d624e99a6db7
Stephen Harrington <stephenharrington04@yahoo.com>
2018-05-08 17:56:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,13 +7,6 @@ class App < Sinatra::Base
     erb :user_input
   end
 
-  post "/" do
-    text_from_user = params[:user_phrase]
-    @piglatinized_text = PigLatinizer.new.piglatinize(text_from_user)
-
-    erb :piglatinize
-  end
-
   post "/piglatinize" do
     text_from_user = params[:user_phrase]
     @piglatinized_text = PigLatinizer.new.piglatinize(text_from_user)
